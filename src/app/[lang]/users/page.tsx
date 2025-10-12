e34fff05319dad3a7e0ec5781225d52164a73ad0
jose alex abad aguilar <jaaa.0698@gmail.com>
2025-10-12 14:21:56 -0500

    refactor: mejorar la estructura y formato del código en page.tsx y actualizar .gitignore

--- src/app/[lang]/users/page.tsx
+++ src/app/[lang]/users/page.tsx
@@ -1,27 +1,28 @@
 import * as React from "react";
-import NextLink from 'next/link';    
+import NextLink from "next/link";
 import TeamMembers from "@/components/Users/TeamMembers";
+import RequireAuth from "@/components/Authentication/RequireAuth";
 
 export default function Page() {
   return (
-    <>
+    <RequireAuth>
       {/* Breadcrumb */}
-      <div className="breadcrumb-card">
+      <div className='breadcrumb-card'>
         <h5>Team Members</h5>
 
-        <ul className="breadcrumb">
+        <ul className='breadcrumb'>
           <li>
-            <NextLink href="/dashboard/ecommerce/">
-              <i className="material-symbols-outlined">home</i>
+            <NextLink href='/dashboard/ecommerce/'>
+              <i className='material-symbols-outlined'>home</i>
               Dashboard
             </NextLink>
-          </li> 
+          </li>
           <li>Users</li>
           <li>Team Members</li>
         </ul>
       </div>
 
       <TeamMembers />
-    </>
+    </RequireAuth>
   );
 }
